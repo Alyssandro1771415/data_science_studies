742dffb82a8d34830e5c3afdf4dc8079492c1c4b
Alyssandro Ramos <dyogo.alyssandro@gmail.com>
2023-10-12 18:56:03 -0300

    12/10

---
+++ Pré-Processamento dos Dados/1 - tipos_de_dados.py
@@ -0,0 +1,17 @@
+"""
+
+variáveis:
+
+    Numérica:
+     Contínua:
+        °Números reais(Temperatura, peso, altura...)
+     Discreta:
+        °Conjunto de valores finitos - Inteiros(Contagem de algo)
+
+    Categórica:
+        Nominal:
+            °Dados não mensuráveis - Sem ordenação(Cor de olhos, gênero, raça...)
+        Ordinal:
+            °Categorizado sob uma ordenação - Tamanho(P, M, G, PP...)
+
+"""
